6de7dbe7adb53ba0cbdf8f5036694423197d470c
Florian König <koenidv@gmail.com>
2023-02-02 19:33:58 +0100

    Remove Spotify callback from generateJwt

--- cloud-functions/generate-jwt.js
+++ cloud-functions/generate-jwt.js
@@ -1,5 +1,5 @@
-const selfUrl = "https://europe-west1-melodymates.cloudfunctions.net/generate-jwt"
-
+const selfUrl =
+  "https://europe-west1-melodymates.cloudfunctions.net/generate-jwt";
 
 /**
  * Responds to any HTTP request.
@@ -7,55 +7,17 @@ const selfUrl = "https://europe-west1-melodymates.cloudfunctions.net/generate-jw
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-export async function spotifyCallback(req, res) {
-  const authcode = req.query.code;
-  const redirectUrl = req.query.cb;
+export async function generateJwt(req, res) {
+  const { access_token } = req.body;
 
-  if (!authcode) {
-    res.status(500).send("Spotify authentication code not specified");
-    return;
-  }
-
-  const response = await spotifyOAuth(authcode);
-  const credentials = {
-    access_token: response.access_token,
-    refresh_token: response.refresh_token,
-    expires: Date.now() + response.expires_in * 1000,
-  };
-
-  if (!credentials.access_token) {
-    res.status(500).send("Failed to resolve Spotify access token");
-    return;
-  }
-
-  const userinfo = await spotifyUserInfo(credentials.access_token)
+  const userinfo = await spotifyUserInfo(access_token);
 
   if (!userinfo.id) {
     res.status(500).send("Failed to get Spotify user info");
     return;
   }
 
-  res.send(userinfo.display_name)
-}
-
-async function spotifyOAuth(code) {
-  const res = await fetch("https://accounts.spotify.com/api/token", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization:
-        "Basic " +
-        new Buffer.from(process.env.SPOTIFY_ID + ":" + process.env.SPOTIFY_SECRET).toString("base64"),
-    },
-    body: new URLSearchParams({
-      code: code,
-      refresh_token: code,
-      redirect_uri: selfUrl,
-      grant_type: "authorization_code",
-    }),
-  });
-  const json = await res.json();
-  return json;
+  res.send(JSON.stringify(userinfo));
 }
 
 async function spotifyUserInfo(access_token) {
@@ -75,6 +37,6 @@ async function spotifyUserInfo(access_token) {
     id: json.id,
     display_name: json.display_name,
     image: image,
-    url: json.external_urls.spotify
-  }
+    url: json.external_urls.spotify,
+  };
 }
