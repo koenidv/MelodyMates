363dcc6ba8df62af414c0a8ffeb20bed8d726b7a
Florian König <koenidv@gmail.com>
2023-02-03 16:10:09 +0100

    Spotify OAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,39 @@
-const SPOTIFY_SCOPES = [
-	'user-read-currently-playing',
-	'user-read-playback-position',
-	'user-read-recently-played',
-	'user-top-read'
-];
-const SPOTIFY_CLIENT_ID = 'f1c9da828b51491dabf6c4df85fbd9ea';
-const SPOTIFY_OAUTH_REDIRECT = 'http://localhost:5173/login/continue';
+import { Buffer as buf } from "buffer";
 
-export const OAUTH_URL = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(
-	SPOTIFY_OAUTH_REDIRECT
-)}&scope=${encodeURIComponent(SPOTIFY_SCOPES.join(' '))}`;
+const SPOTIFY_SCOPES = [
+  "user-read-currently-playing",
+  "user-read-playback-position",
+  "user-read-recently-played",
+  "user-top-read",
+];
+const SPOTIFY_CLIENT_ID = "f1c9da828b51491dabf6c4df85fbd9ea";
+const SPOTIFY_OAUTH_REDIRECT = "http://localhost:5173/login/continue";
+
+export const OAUTH_URL =
+  `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${
+    encodeURIComponent(
+      SPOTIFY_OAUTH_REDIRECT,
+    )
+  }&scope=${encodeURIComponent(SPOTIFY_SCOPES.join(" "))}`;
+
+export async function spotifyOAuth(code: string) {
+  const spotify_secret = import.meta.env.VITE_SPOTIFY_SECRET;
+  const res = await fetch("https://accounts.spotify.com/api/token", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " +
+        buf.from(SPOTIFY_CLIENT_ID + ":" + spotify_secret).toString(
+          "base64",
+        ),
+    },
+    body: new URLSearchParams({
+      code: code,
+      refresh_token: code,
+      redirect_uri: SPOTIFY_OAUTH_REDIRECT,
+      grant_type: "authorization_code",
+    }),
+  });
+  const json = await res.json();
+  return json;
+}
--- src/routes/login/continue/+page.svelte
+++ src/routes/login/continue/+page.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
-    import { page } from "$app/stores";
-    const code = $page.url.searchParams.get("code");
+	import { page } from '$app/stores';
+	import { spotifyOAuth } from '$lib/auth';
+	import { onMount } from 'svelte';
+
+	const code = $page.url.searchParams.get('code');
+	let access_token: string;
+
+    onMount(() => {
+        if (code) {
+            spotifyOAuth(code).then((token) => {
+                access_token = token;
+            });
+        }
+    });
 </script>
 
-<p class="text-xl">
-    Success.
-</p>
-<p>
-    Code is {code}.
-</p>
+{#if !access_token}
+	<p class="text-xl">Loading...</p>
+{:else}
+	<p class="text-xl">Success.</p>
+	<p>
+		Access token is {JSON.stringify(access_token)}.
+	</p>
+{/if}
