6d733649b85eb593fc38e68bc5ca727fb61b9e0a
Florian König <koenidv@gmail.com>
2023-02-05 19:09:44 +0100

    Different spotify observer queue

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -2,12 +2,27 @@ import { get } from "svelte/store";
 import { refreshSpotifyToken } from "$lib/auth";
 import { currentlyPlaying, identity } from "$lib/store";
 
-let intervalId: NodeJS.Timer | undefined = undefined;
+let timeoutId: NodeJS.Timer | null | undefined = undefined;
 
-export function startNowPlayingObserver() {
-  intervalId = setInterval(async () => {
-    queryCurrentSong();
-  }, 10_000);
+export async function startNowPlayingObserver() {
+  timeoutId = undefined;
+  recursiveObserver();
+}
+
+async function recursiveObserver() {
+  const playing = await queryCurrentSong();
+  if (timeoutId === null) return;
+  if (playing) {
+    timeoutId = setTimeout(async () => {
+      recursiveObserver();
+    }, 8_000);
+  } else {
+    await queryLastPlayed();
+    if (timeoutId === null) return;
+    timeoutId = setTimeout(async () => {
+      recursiveObserver();
+    }, 16_000);
+  }
 }
 
 export async function queryCurrentSong() {
@@ -48,9 +63,11 @@ export async function queryCurrentSong() {
     is_playing: json.is_playing,
     timestamp: json.timestamp,
   });
+
+  return true;
 }
 
-export async function queryLastSong() {
+export async function queryLastPlayed() {
   if (!get(identity)?.spotify?.access_token) return;
   await refreshSpotifyToken();
   fetch("https://api.spotify.com/v1/me/player/recently-played?limit=1").then(
@@ -63,6 +80,6 @@ export async function queryLastSong() {
 }
 
 export function stopNowPlayingObserver() {
-  if (intervalId) clearInterval(intervalId);
-  intervalId = undefined;
+  if (timeoutId) clearTimeout(timeoutId);
+  timeoutId = null;
 }
