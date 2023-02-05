34b199dc48f75c461af1378e5f48f069e673ebfc
Florian König <koenidv@gmail.com>
2023-02-05 16:10:58 +0100

    Redirect based on identity

---
+++ src/routes/login/+page.ts
@@ -0,0 +1,10 @@
+import { browser } from "$app/environment";
+import { identity } from "$lib/store";
+import { redirect } from "@sveltejs/kit";
+import { get } from "svelte/store";
+
+export function load() {
+  if (browser && get(identity).jwt) {
+    throw redirect(307, "/home");
+  }
+}
