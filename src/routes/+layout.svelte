72908cc9a8be98069f5ddddf0dd0123a923735e9
Florian König <koenidv@gmail.com>
2023-02-06 18:24:27 +0100

    Login page more responsive whitespace

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="app flex flex-col min-h-full bg-black text-white">
-	<main class="flex flex-col w-full h-full max-w-screen-lg mx-0 my-auto box-border">
+	<main class="flex flex-col w-full h-full max-w-screen-lg m-auto box-border">
 		<slot />
 	</main>
 </div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex flex-col items-center justify-center w-full h-full gap-3 p-4">
     <h1 class="text-4xl font-display">MelodyMates</h1>
-    <p class="mb-32 font-medium">Share music with your mates</p>
+    <p class="mb-[10vh] font-medium">Share music with your mates</p>
     <a href={OAUTH_URL} class="w-full bg-[#1db954] flex items-center justify-center p-3 m-2 rounded-xl"><img class="max-h-8" src="/spotify_logo_white.png" alt="Sign in with Spotify"/></a>
     <p>Sign in with Spotify to continue</p>
 </div>
