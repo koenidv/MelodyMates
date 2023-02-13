a8930e6168e7a27d1a857fdb466f342328f585fe
Florian König <koenidv@gmail.com>
2023-02-13 17:06:36 +0100

    Redirect landing page for now

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,17 @@
 // since there's no dynamic data here, we can prerender
+
+import { browser } from "$app/environment";
+import { identity } from "$lib/store";
+import { redirect } from "@sveltejs/kit";
+import { get } from "svelte/store";
+
 // it so that it gets served as a static asset in production
 export const prerender = true;
+
+export function load() {
+  if (browser && get(identity).jwt) {
+    throw redirect(307, "/home");
+  } else {
+    throw redirect(307, "/login");
+  }
+}
