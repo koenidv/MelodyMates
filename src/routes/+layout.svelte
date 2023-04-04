220a7d636aa24c0be08bb9b56b107176a280ef78
Florian König <koenidv@gmail.com>
2023-04-05 01:32:18 +0200

    Disable text selection everywhere

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="app flex flex-col min-h-full h-0 bg-black text-white">
-	<main class="flex flex-col w-full h-full max-w-screen-md mx-auto box-border">
+	<main class="flex flex-col w-full h-full max-w-screen-md mx-auto box-border select-none">
 		<slot />
 	</main>
 </div>
