432eef71c2c2c80f7a1077251bf766c165f6101a
Florian König <koenidv@gmail.com>
2023-04-06 16:13:42 +0200

    Move login redirect logic to layout

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -3,7 +3,7 @@ import { identity } from "$lib/store";
 import { redirect } from "@sveltejs/kit";
 import { get } from "svelte/store";
 
-export async function load() {
+export function load() {
   if (browser && !get(identity).jwt) {
       throw redirect(307, "/login");
   }
