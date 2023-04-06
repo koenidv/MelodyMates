c7e1a9ffc27260ce2b9159aeae3437e72f781bde
Florian König <koenidv@gmail.com>
2023-04-06 16:09:49 +0200

    Move bottom bar to layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import BottomBar from "$components/BottomBar.svelte";
+	import { startNowPlayingObserver, stopNowPlayingObserver } from "$lib/spotify";
+
+	stopNowPlayingObserver();
+	startNowPlayingObserver();
+</script>
+
+<div class="overflow-x-hidden h-full">
+	<slot />
+	<BottomBar />
+</div>
--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
-	import BottomBar from "$components/BottomBar.svelte";
-	import Feed from "$components/Feed.svelte"
-	import { startNowPlayingObserver, stopNowPlayingObserver } from "$lib/spotify";
-
-	stopNowPlayingObserver();
-	startNowPlayingObserver();
+	import Feed from "$components/Feed.svelte";
 </script>
 
-<div class="overflow-x-hidden h-full">
-	<Feed />
-	<BottomBar />
-</div>
+<Feed />
