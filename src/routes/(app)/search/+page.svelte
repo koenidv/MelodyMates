9d71aca040743f0e44eec5d3137f47db2b67fe37
Florian König <koenidv@gmail.com>
2023-04-08 02:35:17 +0200

    Search page autofocus and close button

--- src/routes/(app)/search/+page.svelte
+++ src/routes/(app)/search/+page.svelte
@@ -2,10 +2,11 @@
 	import { searchSongs } from "$lib/spotify";
 	import debounce from "lodash/debounce";
 	import { searchUsers, type Song } from "$lib/db";
+	import { goto } from "$app/navigation";
 
 	let searchterm = "";
 	let results_spotify: Song[] = [];
-	let results_users: { id: string; name: string; image: string }[] = []
+	let results_users: { id: string; name: string; image: string }[] = [];
 
 	const handleInput = debounce(async (e) => {
 		if (!searchterm) {
@@ -26,19 +27,29 @@
 
 <div class="feed p-4 h-full pb-[4.5rem] overflow-y-auto">
 	<div
-		class="fixed left-4 right-4 top-0 pt-4 bg-gradient-to-b from-black via-black to-transparent z-10">
+		class="fixed flex flex-row gap-4 items-center left-4 right-4 top-0 pt-4 bg-gradient-to-b from-black via-black to-transparent z-10">
+		<!-- svelte-ignore a11y-autofocus -->
 		<input
 			type="text"
+			autofocus
 			placeholder="Search songs"
 			bind:value={searchterm}
 			on:input={handleInput}
 			class="rounded-lg p-2 text-white placeholder-opacity-75 placeholder-white bg-gray-800 h-14 w-full shadow-2xl" />
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<img
+			src="/icons/close.svg"
+			alt="Close"
+			class="h-8 w-8 opacity-90"
+			on:click={() => goto("/home")} />
 	</div>
 
 	<!-- User results -->
 	<div id="users" class="flex flex-row gap-2 mt-[4.5rem] overflow-x-scroll">
 		{#each results_users as user}
-			<a href="/user/{user.id}" class="flex flex-col gap-2 bg-gray-900 w-32 min-w-[8rem] h-24 relative rounded-lg justify-end p-2">
+			<a
+				href="/user/{user.id}"
+				class="flex flex-col gap-2 bg-gray-900 w-32 min-w-[8rem] h-24 relative rounded-lg justify-end p-2">
 				{#if user.image}
 					<img
 						src={user.image}
@@ -51,7 +62,7 @@
 	</div>
 
 	<!-- Song results -->
-	<div id="songs" class="flex flex-col gap-2 mt-4">
+	<div id="songs" class="flex flex-col gap-2 {results_users.length > 0 ? 'mt-4' : ''}">
 		{#each results_spotify as song}
 			<div
 				class="w-full rounded-lg p-2 bg-gray-800 flex flex-row gap-2 relative overflow-clip"
